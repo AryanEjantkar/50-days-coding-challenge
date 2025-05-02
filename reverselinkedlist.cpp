6eefe9008400b27f0b13ea4840ac3814fe8f222c
AryanEjantkar <145706433+AryanEjantkar@users.noreply.github.com>
2025-05-02 12:28:05 +0530

    Add files via upload

---
+++ reverselinkedlist.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = nullptr;
+        ListNode* curr = head;
+
+        while (curr != nullptr) {
+            ListNode* nextNode = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nextNode;
+        }
+
+        return prev;
+    }
+};
+
+// Helper function to print the linked list
+void printList(ListNode* head) {
+    while (head != nullptr) {
+        cout << head->val;
+        if (head->next != nullptr) cout << " -> ";
+        head = head->next;
+    }
+    cout << endl;
+}
+
+// Main function to test reverseList
+int main() {
+    // Creating the list: 1 -> 2 -> 3 -> 4 -> 5
+    ListNode* head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+    head->next->next->next = new ListNode(4);
+    head->next->next->next->next = new ListNode(5);
+
+    cout << "Original List: ";
+    printList(head);
+
+    Solution sol;
+    ListNode* reversedHead = sol.reverseList(head);
+
+    cout << "Reversed List: ";
+    printList(reversedHead);
+
+    return 0;
+}
